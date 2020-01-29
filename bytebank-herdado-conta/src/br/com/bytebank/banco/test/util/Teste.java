2d7a54e411a74fa0b378c3c217e6c0c34eae5520
Tayna Rodrigues <taynajesus@gmail.com>
2020-01-29 13:37:02 -0400

    Classes anônimas

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -1,7 +1,6 @@
 package br.com.bytebank.banco.test.util;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -10,87 +9,77 @@ import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 
-public class TesteComparandoStrings {
+public class Teste {
 
 	public static void main(String[] args) {
-		
+
 		Conta cc1 = new ContaCorrente(22, 33);
 		Cliente clienteCC1 = new Cliente();
 		clienteCC1.setNome("Nico");
 		cc1.setTitular(clienteCC1);
 		cc1.deposita(333.0);
-		
+
 		Conta cc2 = new ContaPoupanca(22, 44);
 		Cliente clienteCC2 = new Cliente();
 		clienteCC2.setNome("Guilherme");
 		cc2.setTitular(clienteCC2);
 		cc2.deposita(444.0);
-		
+
 		Conta cc3 = new ContaPoupanca(22, 11);
 		Cliente clienteCC3 = new Cliente();
 		clienteCC3.setNome("Paulo");
 		cc3.setTitular(clienteCC3);
 		cc3.deposita(222.0);
-		
+
 		Conta cc4 = new ContaPoupanca(22, 22);
 		Cliente clienteCC4 = new Cliente();
 		clienteCC4.setNome("Ana");
 		cc4.setTitular(clienteCC4);
 		cc4.deposita(222.0);
-		
+
 		List<Conta> lista = new ArrayList<Conta>();
 		lista.add(cc1);
 		lista.add(cc2);
 		lista.add(cc3);
 		lista.add(cc4);
-		
-		for(Conta conta : lista) {
-			System.out.println(conta);
+
+		lista.sort(new Comparator<Conta>() {
+
+			@Override
+			public int compare(Conta c1, Conta c2) {
+
+				return Integer.compare(c1.getNumero(), c2.getNumero());
+			}
+
 		}
-		
-	//	NumeroDaContaComparator comparator = new NumeroDaContaComparator();
-		lista.sort(null);//ordem natural
-		
-//		Collections.sort(lista, new NumeroDaContaComparator());
-//		Collections.sort(lista);
-//		Collections.reverse(lista);
-		
-		System.out.println("-------------------------");
-		
-		for(Conta conta : lista) {
+
+	);
+
+		for (Conta conta : lista) {
 			System.out.println(conta + " , " + conta.getTitular().getNome());
 		}
-		
-//		lista.sort(titularComparator);
 	}
-	
-	class TitularDaContaComparator implements Comparator <Conta>{
+
+	class NumerodaContaComparator2 implements Comparator<Conta> {
 
 		@Override
 		public int compare(Conta c1, Conta c2) {
-			
+
+			return Integer.compare(c1.getNumero(), c2.getNumero());
+
+		}
+
+	}
+
+	class TitularDaContaComparator2 implements Comparator<Conta> {
+
+		@Override
+		public int compare(Conta c1, Conta c2) {
+
 			String nomeC1 = c1.getTitular().getNome();
 			String nomeC2 = c2.getTitular().getNome();
 			return nomeC1.compareTo(nomeC2);
 		}
-		
-	}
-	
-	class NumerodaContaComparator implements Comparator<Conta>{
 
-		@Override
-		public int compare(Conta c1, Conta c2) {
-			
-			return Integer.compare(c1.getNumero(), c2.getNumero());
-			
-//			if(c1.getNumero() < c2.getNumero()) {
-//				return -1;
-//			}
-//			if(c1.getNumero() < c2.getNumero()) {
-//				return 1;
-//			}
-//			return 0;
-		}
-		
 	}
 }

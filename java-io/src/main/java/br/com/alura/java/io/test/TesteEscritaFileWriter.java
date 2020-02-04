986a35ac84ee93dfaeea6348eac0ad684f205a53
Tayna Rodrigues <taynajesus@gmail.com>
2020-02-04 11:55:29 -0400

    PrintWriter e PrintStream

---
+++ java-io/src/main/java/br/com/alura/java/io/test/TesteEscritaFileWriter.java
@@ -0,0 +1,31 @@
+package br.com.alura.java.io.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+public class TesteEscritaFileWriter {
+
+	public static void main(String[] args) throws IOException {
+		
+//		//Fluxo de entrada  com arquivo
+//		OutputStream fos = new FileOutputStream("lorem2.txt");
+//		Writer osw = new OutputStreamWriter(fos);
+//		BufferedWriter bw = new BufferedWriter(osw);
+		
+//		FileWriter fw = new FileWriter("lorem2.txt");
+//		BufferedWriter bw = new BufferedWriter(new FileWriter("lorem2.txt"));
+		
+//		PrintStream ps = new PrintStream(new File("lorem.txt"));
+		
+		PrintWriter ps = new PrintWriter("lorem2.txt");
+		
+		ps.println("Tayyyyy");
+		ps.println();
+		ps.println("ohohohoohohohohooh");
+		
+		ps.close();
+	}
+
+}
